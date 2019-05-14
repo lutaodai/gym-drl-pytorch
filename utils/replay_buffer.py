6713e41e529f93cfb67c37524884171235338bd1
lutaodai <lutaodai@hku.hk>
2019-05-14 21:37:20 +0800

    feat: support per

--- utils/replay_buffer.py
+++ utils/replay_buffer.py
@@ -3,6 +3,8 @@ import random
 from collections import namedtuple, deque
 import torch
 
+from utils.SumTree import SumTree
+
 class ReplayBuffer:
     """Fixed-size buffer to store experience tuples."""
 
@@ -43,3 +45,52 @@ class ReplayBuffer:
     def __len__(self):
         """Return the current size of internal memory."""
         return len(self.memory)
+
+class PERBuffer:
+    """ref: https://github.com/rlcode/per/blob/master/prioritized_memory.py"""
+    def __init__(self, size, e=0.01, a=0.6, beta=0.4, beta_increment_per_sampling=0.001):
+        self.size = size
+        self.tree = SumTree(size)
+        
+        self.e = e
+        self.a = a
+        self.beta = beta
+        self.beta_increment_per_sampling = beta_increment_per_sampling
+        
+    def _get_priority(self, error):
+        return (error + self.e) ** self.a
+    
+    def add(self, error, sample):
+        p = self._get_priority(error)
+        self.tree.add(p, sample)
+        
+    def update(self, idx, error):
+        p = self._get_priority(error)
+        self.tree.update(idx, p)
+    
+    def sample(self, n):
+        batch = []
+        idxs = []
+        segment = self.tree.total() / n
+        priorities = []
+
+        self.beta = np.min([1., self.beta + self.beta_increment_per_sampling])
+
+        for i in range(n):
+            a = segment * i
+            b = segment * (i + 1)
+
+            s = random.uniform(a, b)
+            (idx, p, data) = self.tree.get(s)
+            priorities.append(p)
+            batch.append(data)
+            idxs.append(idx)
+
+        sampling_probabilities = priorities / self.tree.total()
+        is_weight = np.power(self.tree.n_entries * sampling_probabilities, -self.beta)
+        is_weight /= is_weight.max()
+
+        return batch, idxs, is_weight
+    
+    def __len__(self):
+        pass
